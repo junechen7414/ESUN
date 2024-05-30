2baea523d6b0d7b3b6dbdf4df6f1fa57a30b733f
junechen7414 <90739416+junechen7414@users.noreply.github.com>
2024-05-30 12:26:55 +0800

    可以勾選多個商品，並設定購買的數量，購買數量不能大於該商品庫存量

--- src/components/showProduct.vue
+++ src/components/showProduct.vue
@@ -4,6 +4,9 @@
         <table class="centered-table">
       <thead>
         <tr>
+          <th>
+            <input type="checkbox" v-model="selectAll" @change="toggleSelectAll(selectAll)" />
+          </th>
           <th>Product Name</th>
           <th>Price</th>
           <th>Quantity</th>
@@ -11,9 +14,15 @@
       </thead>
       <tbody>
         <tr v-for="product in products" :key="product.id">
+          <td>
+            <input type="checkbox" v-model="cart[product.id]" />
+          </td>
           <td>{{ product.name }}</td>
           <td>{{ product.price }}</td>
-          <td>{{ product.quantity }}</td>
+          <td><input type="number" v-model="cart[product.id].quantity"
+            :min="0"
+            :max="product.quantity"
+            @change="updateCartQuantity(product.id,$event.target.value)"></td>
         </tr>
       </tbody>
     </table>
@@ -27,7 +36,9 @@ export default {
   name: 'showProduct',
   data() {
     return {
-      products: []
+      products: [],
+      cart: {},
+      selectAll: false,
     };
   },
   created() {
@@ -38,11 +49,27 @@ export default {
       try {
         const response = await axios.get('http://localhost:8080/showProduct');
         this.products = response.data;
+
+        // Initialize cart with quantity for each product
+        this.cart = this.products.reduce((acc, product) => {
+          acc[product.id] = { quantity: product.quantity };
+          return acc;
+        }, {});
+
       } catch (error) {
         console.error('Error fetching products:', error);
       }
-    }
+    },
+    toggleSelectAll(checked) {
+      this.cart = this.products.reduce((acc, product) => {
+        acc[product.id] = checked; // 將選擇狀態存儲在cart物件中
+        return acc;
+      },{});
+  },
+  updateCartQuantity(productId, quantity) {
+    this.cart[productId].quantity = quantity;
   }
+}
 };
 </script>
 <style>
